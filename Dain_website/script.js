4c950bb7756a7171a1d0bd8c945f241f472bcc99
진승현 <149146164+ynn-i@users.noreply.github.com>
2024-07-24 17:43:54 +0900

    Edit header 웹 컴포넌트로 분리

--- Dain_website/script.js
+++ Dain_website/script.js
@@ -15,3 +15,74 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     };
 });
+
+// header
+class HeaderComponent extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+        <link rel="stylesheet" href="../../styles.css">
+        <header>
+            <nav>
+                <a class="logo" href="./index.html"><img src="./images/logo.svg" alt="Logo" /></a>
+                <button class="header-modal-btn">
+                    PROJECTS
+                    <img class="icon-32" src="./images/arr_down.svg" alt="Dropdown Arrow" />
+                </button>
+                <ul class="header-modal hidden">
+                    <li>
+                        <a href="#">
+                            <div>
+                                <p class="header-modal-title">CHARMSAE</p>
+                                <span>BX Design</span>
+                            </div>
+                            <img class="icon-32" src="./images/arr_right.svg" alt="Right Arrow" />
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <div>
+                                <p class="header-modal-title">CHARMSAE</p>
+                                <span>BX Design</span>
+                            </div>
+                            <img class="icon-32" src="./images/arr_right.svg" alt="Right Arrow" />
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <div>
+                                <p class="header-modal-title">CHARMSAE</p>
+                                <span>BX Design</span>
+                            </div>
+                            <img class="icon-32" src="./images/arr_right.svg" alt="Right Arrow" />
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <div>
+                                <p class="header-modal-title">CHARMSAE</p>
+                                <span>BX Design</span>
+                            </div>
+                            <img class="icon-32" src="./images/arr_right.svg" alt="Right Arrow" />
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <div>
+                                <p class="header-modal-title">CHARMSAE</p>
+                                <span>BX Design</span>
+                            </div>
+                            <img class="icon-32" src="./images/arr_right.svg" alt="Right Arrow" />
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+        `;
+
+        this.querySelector('.header-modal-btn').addEventListener('click', () => {
+            this.querySelector('.header-modal').classList.toggle('hidden');
+        });
+    }
+}
+
+customElements.define('header-component', HeaderComponent);
